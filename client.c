601e9283fd9ae9e7aa1ac448e6387a3e64d0fb02
yeawonbong <ybong.develop@gmail.com>
2021-07-14 02:10:49 +0900

    sending_signals

--- client.c
+++ client.c
@@ -1,16 +1,54 @@
 #include "minitalk.h"
 
+void	send_end(int spid)
+{
+	int i;
+
+	i = 8;
+	while (i--)
+		kill(spid, SIGUSR1);
+}
+
+void	send_number(int spid, int num)
+{
+	int	temp;
+	int	len;
+
+	temp = num;
+	len = 1;
+	// while (temp && (temp /= 10))
+	// 	len++;
+	// printf("len is %d\n", len);
+	while(num)
+	{
+		if (num & 1)
+		{
+			kill(spid, SIGUSR2);
+			printf("SENT 1\n");
+		}
+		else
+		{
+			kill(spid, SIGUSR1);
+			printf("SETNT 0\n");
+		}
+		num = num >> 1;
+		// printf("shifted num : %d\n", num);
+	}
+}
+
+
 int main(int argc, char *argv[])
 {
-	// int spid;
+	int spid;
 	int cpid;
 
-	// spid = atoi(argv[1];
+	spid = atoi(argv[1]);
 	if (argc != 3)
 		exit(EXIT_FAILURE);
 	cpid = getpid();
 	kill(ft_atoi(argv[1]), SIGUSR1);
 	printf("my pid : %d\n", cpid);
-	printf("binary in bit : %d\n", cpid & 1);
+	printf("binary in bit : %d\n", 1 | 0);
+	send_number(spid, 10);
 	return(0);
 }
--- server.c
+++ server.c
@@ -7,6 +7,7 @@
 
 void	write_str(int i)
 {
+	printf("I GOT SIGUSR1! \n");
 	i = 0;
 	i++;
 	ft_putstr_fd("client pid : ", STDOUT_FILENO);
@@ -16,10 +17,11 @@ int	main(void)
 {
 	int	pid;
 	// char	*c;
-	struct sigaction sig1;
+	static struct sigaction sig1;
 	// struct sigaction sig2;
 
 	sig1.sa_handler = write_str;
+	sig1.sa_flags = SA_SIGINFO;
 	ft_putstr_fd("server pid : ", STDOUT_FILENO);
 	ft_putnbr_fd(pid = getpid(), STDOUT_FILENO);
 	ft_putstr_fd("\n", STDOUT_FILENO);
@@ -29,6 +31,9 @@ int	main(void)
 		printf("signal(SIGUSR1) Error\n");
 		return -1;
 	}
+	else
+		printf("d\n", sig1.sa_flags.si_pid);
+	// ft_putstr_fd(ft_itoa(sig1.sa_flags.si_pid), STDOUT_FILENO);
 	while (1)
 	{
 	}
