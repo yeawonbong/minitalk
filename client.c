6d3b4c9d6697cf00845ea1dcdd1b445450dff9c8
yeawonbong <ybong.develop@gmail.com>
2021-07-17 04:44:04 +0900

    0717_interaction구현

--- client.c
+++ client.c
@@ -1,5 +1,8 @@
 #include "minitalk.h"
 
+static int	g_spid;
+static int	g_num;
+
 void	send_end(int spid)
 {
 	int i;
@@ -9,106 +12,44 @@ void	send_end(int spid)
 		kill(spid, SIGUSR1);
 }
 
-void	send_sig1(int signo, siginfo_t *siginfo, void *none)
+void	ft_send_number(int signo)
 {
-	printf("GOT SIG__send1\n");
-	kill(siginfo->si_pid, SIGUSR1);
-}
-
-void	send_sig2(int signo, siginfo_t *siginfo, void *none)
-{
-	printf("GOT SIG__send2\n");
-	kill(siginfo->si_pid, SIGUSR2);
-}
-
-
-void	ft_send_number(int spid)
-{
-	int	num;
-	int	len;
-
-	num = 10;
-	//2 len = 1;
-	// while (temp && (temp /= 10))
-	// 	len++;
-	// printf("len is %d\n", len);
-	while(num)
+	while(g_num)
 	{
-		printf("num : %d, %d\n", num, num & 1);
-		if (num & 1)
+		printf("g_num : %d, %d\n", g_num, g_num & 1);
+		if (g_num & 1)
 		{
-			// sigaction(SIGUSR1, &sig2, 0);
-			kill(spid, SIGUSR2);
+			kill(g_spid, SIGUSR2);
 			printf("SENT 1\n");
 		}
-		else if ((num & 1) == 0)
+		else if ((g_num & 1) == 0)
 		{
-			// sigaction(SIGUSR1, &sig1, 0);
-			kill(spid, SIGUSR1);
+			kill(g_spid, SIGUSR1);
 			printf("SETNT 0\n");
 		}
-		num = num >> 1;
+		g_num = g_num >> 1;
+		printf("나간다\n");
+		return ;
 	}
-	return ;
-}
-
-
-void	send_number(int signo, siginfo_t *siginfo, void *none)
-{
-	ft_send_number(siginfo->si_pid);
-	// int	num;
-	// int	len;
-	// struct sigaction sig1;
-	// struct sigaction sig2;
-
-
-	// num = 10;
-	// //2 len = 1;
-	// // while (temp && (temp /= 10))
-	// // 	len++;
-	// // printf("len is %d\n", len);
-	// sig1.sa_sigaction = send_sig1;
-	// sig2.sa_sigaction = send_sig2;
-	// sig1.sa_flags = SA_SIGINFO;
-	// sig2.sa_flags = SA_SIGINFO;
-	// while(num)
-	// {
-	// 	printf("num : %d\n", num);
-	// 	pause();
-	// 	if (num & 1)
-	// 	{
-	// 		// sigaction(SIGUSR1, &sig2, 0);
-	// 		kill(siginfo->si_pid, SIGUSR2);
-	// 		printf("SENT 1\n");
-	// 	}
-	// 	else
-	// 	{
-	// 		// sigaction(SIGUSR1, &sig1, 0);
-	// 		kill(siginfo->si_pid, SIGUSR1);
-	// 		printf("SETNT 0\n");
-	// 	}
-	// 	num = num >> 1;
-	// }
+	exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char *argv[])
 {
-	int spid;
-	int cpid;
 	struct sigaction sig1;
-
-	sig1.sa_sigaction = send_number;
-	sig1.sa_flags = SA_SIGINFO;
+	g_num = 10;
+	printf("first g_num : %d\n", g_num);
 	if (argc != 3)
 		exit(EXIT_FAILURE);
-	spid = ft_atoi(argv[1]);
-	cpid = getpid();
-	// kill(spid, SIGUSR1);
-	printf("my pid : %d\n", cpid);
-	// pause();
-	// sigaction(SIGUSR1, &sig1, 0);
-	ft_send_number(spid);
-	// while(1)
-	// {}
+	if (!g_spid)
+	{
+		g_spid = ft_atoi(argv[1]);
+		kill(g_spid, SIGUSR1);
+	}
+	printf("my pid : %d\n", getpid());
+	signal(SIGUSR1, &ft_send_number);
+	printf("넘버: %d\n", g_num);
+	while(g_num)
+	{}
 	return(0);
 }
--- server.c
+++ server.c
@@ -2,56 +2,28 @@
 
 static int	g_currclient;
 
-void	ft_get_sig(int signo, siginfo_t *siginfo, void *none)
-{printf("[----IN_GETSIG]\n");
-	if (signo == SIG1)
-	{
-		printf("GOT SIGUSR1!\nSIGNO : %d\n", signo);
-	}
-	else if (signo == SIG2)
-		printf("GOT SIGUSR2!\nSIGNO : %d\n", signo);
-	// kill(siginfo->si_pid, SIGUSR1);
-
-printf("[--EXIT_GETSIG]\n");
-	return ;
-}
 
 void	ft_connect(int signo, siginfo_t *siginfo, void *none)
 {
 	printf("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡINㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n");
-
-	printf("!!!!!!!!!si_pid : %d\n", siginfo->si_pid);
-	if (!g_currclient)
+	printf("---currclient: %d\n---si_pid is : %d\n", g_currclient, siginfo->si_pid);
+	if (siginfo->si_pid != g_currclient) //나중에는 8bit 0일때 g_currclient = 0으로 바꿔주고, 0일경우로 조건 넣어주기 (다른 client 차단해야함)
 	{
 		g_currclient = siginfo->si_pid;
-	}
-	printf("---currclient: %d\n---Client is : %d\n", g_currclient, siginfo->si_pid);
-	// if (g_currclient != siginfo->si_pid)
-	// {
-	// 	printf("DIFF\n");
-	// 	g_currclient = 0;
-	// 	return;
-	// }
-	// static struct sigaction get_sig;
-	// get_sig.sa_sigaction = ft_get_sig;
-	// get_sig.sa_flags = SA_SIGINFO;
-	if (siginfo->si_pid != g_currclient)
-	{
 		ft_putstr_fd("Successfully Connected!\n> Client PID : ", STDOUT_FILENO);
 		ft_putnbr_fd(siginfo->si_pid, STDOUT_FILENO);
 		ft_putchar_fd('\n', STDOUT_FILENO);
 	}
-	ft_get_sig(signo, siginfo, none);
-	// kill(siginfo->si_pid, SIGUSR1);
-	// sigaction(SIGUSR1, &get_sig, 0);
-	// sigaction(SIGUSR2, &get_sig, 0);
-	printf("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡOUT : %dㅡㅡㅡㅡㅡㅡ\n", signo);
-	// pause();
-	// if (sigaction(SIGUSR1, &get_sig, 0) == -1 || sigaction(SIGUSR2, &get_sig, 0) == -1) //어느것이든 일단 들어간다. 
-	// {
-	// 	printf("signal(SIGUSR1) Error\n");
-	// 	return ;
-	// }
+	else
+	{
+		if (signo == SIG1)
+		{
+			printf("GOT SIGUSR1!\nSIGNO : %d\n", signo);
+		}
+		else if (signo == SIG2)
+			printf("GOT SIGUSR2!\nSIGNO : %d\n", signo);
+	}
+	kill(siginfo->si_pid, SIGUSR1);
 	return ;
 }
 
@@ -64,23 +36,14 @@ void	ft_launch()
 
 int	main(void)
 {
-	// static int	currclient =50;
-	// char	*c;
-	struct sigaction connect; //static? 
-	// struct sigaction sig2;
+	static struct sigaction connect;
 
 	connect.sa_sigaction = ft_connect;
 	connect.sa_flags = SA_SIGINFO;
 	ft_launch();
-	sigaction(SIGUSR1, &connect, 0);
-	sigaction(SIGUSR2, &connect, 0);
-	// if (sigaction(SIGUSR1, &connect, 0) == -1 || sigaction(SIGUSR2, &connect, 0) == -1) //어느것이든 일단 들어간다. 
-	// {
-	// 	printf("signal(SIGUSR1) Error\n");
-	// 	return -1;
-	// }
+	sigaction(SIGUSR1, &connect, NULL);
+	sigaction(SIGUSR2, &connect, NULL);
 	while (1)
 	{
 	}
 }
-
