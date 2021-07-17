08d9a49e1c0800c004d38ba1526d8fe6037256f9
yeawonbong <ybong.develop@gmail.com>
2021-07-17 17:00:26 +0900

    sending_char_success

--- client.c
+++ client.c
@@ -1,55 +1,66 @@
 #include "minitalk.h"
 
 static int	g_spid;
-static int	g_num;
+static int	g_byte; //8bit
+static int	g_idx;
+static int	g_count;
 
-void	send_end(int spid)
+void	send_end(void)
 {
 	int i;
 
 	i = 8;
 	while (i--)
-		kill(spid, SIGUSR1);
+		kill(g_spid, SIGUSR1);
 }
 
 void	ft_send_number(int signo)
 {
-	while(g_num)
+printf("GOT_SIGNAL\n");	
+	if(g_count < 8)
 	{
-		printf("g_num : %d, %d\n", g_num, g_num & 1);
-		if (g_num & 1)
+		printf("g_byte : %d, %d\n", g_byte, g_byte & 1);
+		if (g_byte & 1)
 		{
 			kill(g_spid, SIGUSR2);
-			printf("SENT 1\n");
+			printf("-----------SENT 1\n");
 		}
-		else if ((g_num & 1) == 0)
+		else if ((g_byte & 1) == 0)
 		{
 			kill(g_spid, SIGUSR1);
-			printf("SETNT 0\n");
+			printf("-----------SETNT 0\n");
 		}
-		g_num = g_num >> 1;
-		printf("나간다\n");
+		g_byte = g_byte >> 1;
+		g_count++;
 		return ;
 	}
+	g_idx++;
+	printf("EXIT\n");
+	send_end();
 	exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char *argv[])
 {
 	struct sigaction sig1;
-	g_num = 10;
-	printf("first g_num : %d\n", g_num);
+
+	g_byte = (int)argv[2][g_idx];
+	printf("first g_byte : %d\n", g_byte);
 	if (argc != 3)
+	{
+		ft_putstr_fd("Error : Invalid Input\n", STDOUT_FILENO);
 		exit(EXIT_FAILURE);
+	}
 	if (!g_spid)
 	{
 		g_spid = ft_atoi(argv[1]);
-		kill(g_spid, SIGUSR1);
+		kill(g_spid, SIGUSR1); // 처음 연결확인 signal
 	}
 	printf("my pid : %d\n", getpid());
 	signal(SIGUSR1, &ft_send_number);
-	printf("넘버: %d\n", g_num);
-	while(g_num)
-	{}
+	printf("넘버: %d\n", g_byte);
+	while (argv[2][g_idx] && g_count < 8)
+	{
+	}
 	return(0);
 }
--- server.c
+++ server.c
@@ -1,30 +1,55 @@
 #include "minitalk.h"
 
-static int	g_currclient;
-
+static int	g_currclient; // 전역 구조체 변수로 만들기
+static int	g_count;
+static int	g_byte;
+static int	g_add;
 
 void	ft_connect(int signo, siginfo_t *siginfo, void *none)
 {
 	printf("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡINㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n");
 	printf("---currclient: %d\n---si_pid is : %d\n", g_currclient, siginfo->si_pid);
+
+
 	if (siginfo->si_pid != g_currclient) //나중에는 8bit 0일때 g_currclient = 0으로 바꿔주고, 0일경우로 조건 넣어주기 (다른 client 차단해야함)
 	{
 		g_currclient = siginfo->si_pid;
 		ft_putstr_fd("Successfully Connected!\n> Client PID : ", STDOUT_FILENO);
 		ft_putnbr_fd(siginfo->si_pid, STDOUT_FILENO);
 		ft_putchar_fd('\n', STDOUT_FILENO);
+		g_count = 8; // 초기화 함수 짜기, 구조체로 만들기
+		g_byte = 0;
+		g_add = 1; // 2^8
 	}
 	else
 	{
 		if (signo == SIG1)
 		{
-			printf("GOT SIGUSR1!\nSIGNO : %d\n", signo);
+			// g_byte[id--] = '' 
+			printf("GOT SIGUSR1! -----SIGNO : %d\n", signo - 30);
 		}
 		else if (signo == SIG2)
-			printf("GOT SIGUSR2!\nSIGNO : %d\n", signo);
+		{
+			g_byte += g_add;
+			printf("GOT SIGUSR2! -----SIGNO : %d\n", signo - 30);
+		}
+		printf("CUR_ADD : %d //// CUR_NUM : %d\n", g_add, g_byte);
+		g_add *= 2;
+		printf("BYTE COUNT__%d\n",g_count);
+		g_count--;
+	}
+	if (g_count == 0)
+	{
+		if (g_byte)
+			ft_putchar_fd(g_byte, STDOUT_FILENO);
+		else
+		{
+			g_currclient = 0;
+			ft_putstr_fd("\nGot all the signals, Disconnected!\n", STDOUT_FILENO);
+		}
+		return ;
 	}
 	kill(siginfo->si_pid, SIGUSR1);
-	return ;
 }
 
 void	ft_launch()
