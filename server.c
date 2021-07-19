bfb4aa922b805fcbefbd84c871c4d41a86a03d48
ybong <ybong@ybongui-MacBookPro.local>
2021-07-20 02:16:15 +0900

    revise_simple_ver

--- server.c
+++ server.c
@@ -6,7 +6,7 @@ void	ft_init(void)
 {
 	g_server.count = 8; // 초기화 함수 짜기, 구조체로 만들기
 	g_server.byte = 0;
-	g_server.add = 1; // 2^8
+	g_server.add = 1; // 2^0
 }
 
 
@@ -20,20 +20,18 @@ void	ft_connect(int signo, siginfo_t *siginfo, void *none)
 	{
  //나중에는 8bit 0일때 g_server.currclient = 0으로 바꿔주고, 0일경우로 조건 넣어주기 (다른 client 차단해야함)
 		g_server.currclient = siginfo->si_pid;
-		ft_putstr_fd("Successfully Connected!\n> Client PID : ", STDOUT_FILENO);
+		ft_putstr_fd("Client PID : ", STDOUT_FILENO);
 		ft_putnbr_fd(siginfo->si_pid, STDOUT_FILENO);
 		ft_putchar_fd('\n', STDOUT_FILENO);
 		ft_init();
 	}
-	else // 여기에 조건 curr_client와 같을때만 넣어주기. 
-	{
-		g_server.byte += g_server.add * (signo - 30);
+	// 여기에 조건 curr_client와 같을때만 넣어주기. 
+	g_server.byte += g_server.add * (signo - 30);
 		// printf("GOT SIGUSR! -----SIGNO : %d\n", signo - 30);
 		// printf("CUR_ADD : %d //// CUR_NUM : %d\n", g_server.add, g_server.byte);
 		// printf("BYTE COUNT__%d\n",g_server.count);
-		g_server.add *= 2;
-		g_server.count--;
-	}
+	g_server.add *= 2;
+	g_server.count--;
 	if (g_server.count == 0)
 	{
 		// printf("문자하나끝남\n");
@@ -44,14 +42,14 @@ void	ft_connect(int signo, siginfo_t *siginfo, void *none)
 		}
 		else
 		{
+			ft_putstr_fd("\nDisconnected!\n", STDOUT_FILENO);
+			usleep(100);
+			kill(g_server.currclient, SIGUSR2); // Disconnect
+			printf("sent_signal2\n");
 			g_server.currclient = 0;
-			ft_putstr_fd("\nGot all the signals, Disconnected!\n", STDOUT_FILENO);
-			// kill(g_server.currclient, SIGUSR2); // Disconnect
-			return ;
+			ft_init();
 		}
 	}
-	// printf("시그널보내\n");
-	kill(siginfo->si_pid, SIGUSR1);
 	return ;
 }
 
