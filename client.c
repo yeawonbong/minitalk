4fb0fe278df13160b67fcd1edd6fbc5f3948d0f8
ybong <ybong@ybongui-MacBookPro.local>
2021-07-23 15:16:21 +0900

    process

--- client.c
+++ client.c
@@ -4,7 +4,6 @@ static t_client g_client;
 
 void	ft_disconnect(int signo)
 {
-	// ft_putchar_fd('0', STDOUT_FILENO);//
 	signo++;
 	ft_putstr_fd("Sent all the signals, Disconnected!\n", STDOUT_FILENO);
 	exit(EXIT_SUCCESS);
@@ -12,9 +11,7 @@ void	ft_disconnect(int signo)
 
 void	ft_send_number(int signo)
 {
-	// ft_putchar_fd('.', STDOUT_FILENO);//
 	signo++; // 이거 빼고 TO_SIGNAL 대체해도 됨 
-	// printf("시그널보낸다. %d\n", g_client.byte & 1);
 	usleep(50);
 	kill(g_client.spid, (g_client.byte & 1) + TO_SIGNAL); //signo (30)
 	g_client.byte = g_client.byte >> 1;
@@ -33,18 +30,13 @@ void	ft_send_number(int signo)
 			g_client.byte = 0;
 			g_client.null = 1; //얘를 지금 잠시 주석해놨음. 
 			g_client.count = 0;
-	// ft_putchar_fd('0', STDOUT_FILENO);//
 		}
 		return ;
 	}
-	// if (!g_client.null)
-	// 	return ;
-	// printf("loop\n");
 	while(1)
 	{
 		signal(SIGUSR2, &ft_disconnect);
 	}
-	// ft_disconnect();
 }
 
 int main(int argc, char *argv[])
@@ -61,9 +53,7 @@ int main(int argc, char *argv[])
 		g_client.spid = ft_atoi(argv[1]);
 		usleep(50);
 		kill(g_client.spid, SIGUSR1); // 처음 연결확인 signal
-		// ft_putstr_fd("시그널보낸다\n", STDOUT_FILENO);
 	}
-	// printf("my pid : %d\n", getpid());
 	while (1)
 	{
 		signal(SIGUSR1, &ft_send_number);
--- server.c
+++ server.c
@@ -15,7 +15,6 @@ void	ft_init(void)
 
 void	ft_connect(int signo, siginfo_t *siginfo, void *none)
 {
-	// ft_putchar_fd('.', STDOUT_FILENO);//
 	none++;
 	if (!g_currclient) // Client 처음 연결 signal
 	{
@@ -25,9 +24,6 @@ void	ft_connect(int signo, siginfo_t *siginfo, void *none)
 		ft_putnbr_fd(siginfo->si_pid, STDOUT_FILENO);
 		ft_putchar_fd('\n', STDOUT_FILENO);
 		ft_init();
-		// usleep(50);
-		// kill(g_currclient, SIGUSR1);
-		// return ;
 	}
 	else if (siginfo->si_pid == g_currclient)
 	{
@@ -37,15 +33,10 @@ void	ft_connect(int signo, siginfo_t *siginfo, void *none)
 	// }
 		if (g_count == 0)
 		{
-			// ft_putstr_fd("문자하나끝남\n", STDOUT_FILENO);
 			if (g_byte) // NULL이 아니면
 			{
-				// ft_putstr_fd("----------------------", STDOUT_FILENO);
 				ft_putchar_fd(g_byte, STDOUT_FILENO);
-				// printf("\n");
 				ft_init();
-				// kill(siginfo->si_pid, SIGUSR1);
-				// return ;
 			}
 			else
 			{
@@ -58,7 +49,6 @@ void	ft_connect(int signo, siginfo_t *siginfo, void *none)
 			}
 		}
 	}
-	// ft_putstr_fd("시그널보낸다\n", STDOUT_FILENO);
 	usleep(50);
 	kill(siginfo->si_pid, SIGUSR1);
 	return ;
